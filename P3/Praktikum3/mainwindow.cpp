d89f5e7ffdf9fcba7dea70a6ce8f54a4d617f8f4
Tobi <opel.tobias@gmx.de>
2017-05-03 16:11:08 +0200

    FIX: TO - Reimport & Umorganisation

---
+++ P3/Praktikum3/mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QObject::connect(ui->myglwid, SIGNAL(sendZOffset(int)),
+                     ui->sbxScaleZ, SLOT(setValue(int)));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
